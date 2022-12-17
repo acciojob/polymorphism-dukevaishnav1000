3a3fa0044b03309fea0cbe3573915c6d7f2776d5
dukevaishnav1000 <dukevaishnav3853@gmail.com>
2022-12-17 18:40:00 +0530

    making class static

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static class Product{
 
+        public int product(int x, int y) {
+
+            return x*y;
+        }
+
+        public int product(int x, int y,int z) {
+
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+
+            return x*y;
+        }
+
+
+    }
     public static void main(String[] args) {
 
         Product p=new Product();
@@ -15,22 +33,3 @@ public class Main {
 
 }
 
-class Product{
-
-    public int product(int x, int y) {
-
-        return x*y;
-    }
-
-    public int product(int x, int y,int z) {
-
-        return x*y*z;
-    }
-
-    public double product(double x, double y) {
-
-        return x*y;
-    }
-
-
-}
